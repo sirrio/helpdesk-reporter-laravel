92446e1ebf2ff4d5f91e16b7bbf7bc2ed2348bf7
David Martin <home@sirrio.de>
2022-06-23 18:48:32 +0200

    changed migration: ismod not to default to true

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-            $table->boolean('isMod')->default(true);
+            $table->boolean('isMod')->default(false);
             $table->boolean('isAdmin')->default(false);
         });
     }
